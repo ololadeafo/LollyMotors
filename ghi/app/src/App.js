9bbbe0adf1754bb7fbbf147b9e75e439b4f947af
Ross <rappel610@gmail.com>
2023-03-07 10:09:38 -0500

    Added automobile list and form pages

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,14 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
+import AutoMobileForm from './automobileForm';
+import AutomobileList from './automobileList';
 
-function App() {
+function App(props) {
   return (
     <BrowserRouter>
       <Nav />
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="automobiles">
+            <Route path= "" element={<AutomobileList automobiles={props.automobiles}/>}/>
+            <Route path= "new" element={<AutoMobileForm />} />
+          </Route>
         </Routes>
       </div>
     </BrowserRouter>
