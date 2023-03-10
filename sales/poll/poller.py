993b599397cbdb08093cebbdff47071e7f650133
Ross <rappel610@gmail.com>
2023-03-10 15:02:36 -0500

    Final commit, got rid of unused code and print statements and anything that was commented out

--- sales/poll/poller.py
+++ sales/poll/poller.py
@@ -9,8 +9,7 @@ sys.path.append("")
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sales_project.settings")
 django.setup()
 
-# Import models from sales_rest, here.
-# from sales_rest.models import Something
+
 from sales_rest.models import AutomobileVO
 
 def get_automobiles():
